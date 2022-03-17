df467b1fce960c0fbc045fcf643f6841f3534e3f
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-03-17 14:05:54 +0530

    fix: patch

--- non_profit/patches/rename_non_profit_fields.py
+++ non_profit/patches/rename_non_profit_fields.py
@@ -8,8 +8,8 @@ def execute():
 		rename_field("Donation", "razorpay_payment_id", "payment_id")
 
 	if frappe.db.table_exists("Tax Exemption 80G Certificate"):
-		frappe.reload_doc("regional", "doctype", "Tax Exemption 80G Certificate")
-		frappe.reload_doc("regional", "doctype", "Tax Exemption 80G Certificate Detail")
+		frappe.reload_doc("non_profit", "doctype", "Tax Exemption 80G Certificate")
+		frappe.reload_doc("non_profit", "doctype", "Tax Exemption 80G Certificate Detail")
 
 		rename_field("Tax Exemption 80G Certificate", "razorpay_payment_id", "payment_id")
 		rename_field("Tax Exemption 80G Certificate Detail", "razorpay_payment_id", "payment_id")
