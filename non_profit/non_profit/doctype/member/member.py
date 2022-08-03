8e360e5856916884017b158e295df3cbbc42f93e
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-08-03 11:01:46 +0530

    fix: update payment gateway controller path

--- non_profit/non_profit/doctype/member/member.py
+++ non_profit/non_profit/doctype/member/member.py
@@ -5,9 +5,10 @@
 import frappe
 from frappe import _
 from frappe.contacts.address_and_contact import load_address_and_contact
-from frappe.integrations.utils import get_payment_gateway_controller
 from frappe.model.document import Document
 from frappe.utils import cint, get_link_to_form
+
+from payments.utils import get_payment_gateway_controller
 
 from non_profit.non_profit.doctype.membership_type.membership_type import get_membership_type
 
--- non_profit/non_profit/doctype/non_profit_settings/non_profit_settings.py
+++ non_profit/non_profit/doctype/non_profit_settings/non_profit_settings.py
@@ -4,9 +4,9 @@
 
 import frappe
 from frappe import _
-from frappe.integrations.utils import get_payment_gateway_controller
 from frappe.model.document import Document
 
+from payments.utils import get_payment_gateway_controller
 
 class NonProfitSettings(Document):
 	@frappe.whitelist()
